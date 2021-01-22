c0ed8c6ec4f174adfac16d775c31aa2f7f7a152d
Felipe <felipequaquio@gmail.com>
2021-01-21 21:14:03 -0300

    Criada conexão com o mongodb utilizando o mongoose

---
+++ src/main/orm/mongoose/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const env = require('../../config/environment')
+const dbUri = env.DB_URI
+const dbName = env.DB_NAME
+
+mongoose.connect(`${dbUri}/${dbName}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'Connection error: '))
+db.on('connected', console.info.bind(console, 'Database sucessfully connected'))
+
+module.exports = mongoose
