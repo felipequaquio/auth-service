e8cb6e9279ed2c13212446bef4b3aec2575ac298
Felipe <felipequaquio@gmail.com>
2021-01-22 14:09:59 -0300

    Alterada pasta do arquivo app.js para seguir boas práticas de modelos arquiteturais. Utilizados midlewares para validar endpoints e tipo de conteúdo enviado via http

---
+++ src/infrastructure/config/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const routes = require('../routes')
+const invalidEndpoint = require('../midlewares/invalid-endpoint')
+const requestTypesAcceptable = require('../midlewares/requests-types-acceptable')
+const app = express()
+
+app.disable('x-powered-by')
+app.use(express.json())
+app.use(requestTypesAcceptable)
+app.use(routes)
+app.use(invalidEndpoint)
+
+module.exports = app
