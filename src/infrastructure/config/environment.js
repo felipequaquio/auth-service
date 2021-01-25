fe4daa9af4658913a1404c43d0adae515614b203
Felipe <felipequaquio@gmail.com>
2021-01-25 17:38:15 -0300

    Alteradas configurações de ambiente para obter a URI do MongoDB dinâmica, conforme utiliza ou não o Docker

--- src/infrastructure/config/environment.js
+++ src/infrastructure/config/environment.js
@@ -1,8 +1,17 @@
 require('dotenv').config()
 
+const APP_PORT = process.env.APP_PORT
+const DB_LOCAL_URI = process.env.DB_LOCAL_URI
+const DB_LOCAL_DOCKER_URI = process.env.DB_LOCAL_DOCKER_URI
+const DB_NAME = process.env.DB_NAME
+const JWT_SECRET = process.env.JWT_SECRET
+const USING_DOCKER = process.env.USING_DOCKER
+const DB_URI = USING_DOCKER === 'true' ? DB_LOCAL_DOCKER_URI : DB_LOCAL_URI
+
 module.exports = {
-  APP_PORT: process.env.APP_PORT,
-  DB_URI: process.env.DB_URI,
-  DB_NAME: process.env.DB_NAME,
-  JWT_SECRET: process.env.JWT_SECRET
+  APP_PORT: APP_PORT,
+  DB_URI: DB_URI,
+  DB_NAME: DB_NAME,
+  JWT_SECRET: JWT_SECRET,
+  USING_DOCKER: USING_DOCKER
 }
