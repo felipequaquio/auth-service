86e0e2ffaa54751d7c4c824a0e72747f5c211de8
Felipe <felipequaquio@gmail.com>
2021-01-26 01:53:06 -0300

    Alterado diretório dos testes de caso de uso de criar usuários e criado testes unitários, antes eram de integração

---
+++ tests/create-user-use-case.test.js
@@ -0,0 +1,76 @@
+const CreateUserUseCase = require('../src/application/use-cases/create-user-use-case')
+const MissingParamError = require('../src/utils/errors/missing-param-error')
+const TokenHandler = require('../src/application/security/token-handler')
+const PasswordEncrypter = require('../src/application/security/password-encrypter')
+const UserRepository = require('../src/infrastructure/repositories/user-repository')
+
+describe('Create user use case tests', () => {
+  let createUserUseCase
+
+  beforeEach(() => {
+    createUserUseCase = new CreateUserUseCase(
+      new UserRepository(),
+      new TokenHandler(),
+      new PasswordEncrypter()
+    )
+  })
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  describe('Success', () => {
+    test('Should create user and return user created with token', async () => {
+      const userData = {
+        nome: 'Teste',
+        email: 'mail@mail.com',
+        senha: '123abc',
+        telefones: []
+      }
+      jest.spyOn(UserRepository.prototype, 'findUserByEmail').mockReturnValue(null)
+      jest.spyOn(TokenHandler.prototype, 'generate').mockReturnValue('token')
+      jest.spyOn(UserRepository.prototype, 'create').mockReturnValue({
+        id: 'id',
+        data_criacao: 'data criacao',
+        data_atualizacao: 'data atualizacao',
+        token: 'token-generated'
+      })
+
+      const result = await createUserUseCase.create(userData)
+
+      expect(result).toHaveProperty('token')
+      expect(result.token).toBe('token-generated')
+    })
+
+    test('Should return null when create user if user exists', async () => {
+      const userData = {
+        nome: 'Teste',
+        email: 'mail@mail.com',
+        senha: '123abc',
+        telefones: []
+      }
+
+      jest.spyOn(UserRepository.prototype, 'findUserByEmail').mockReturnValue({ email: 'mail@mail.com' })
+
+      const result = await createUserUseCase.create(userData)
+
+      expect(result).toBe(null)
+    })
+  })
+
+  describe('Error', () => {
+    test('Should return error when missing param error', async () => {
+      const userData = {
+        nome: 'Teste',
+        email: 'mail@mail.com',
+        senha: '123abc'
+      }
+
+      try {
+        await createUserUseCase.create(userData)
+      } catch (error) {
+        expect(error).toBeInstanceOf(MissingParamError)
+      }
+    })
+  })
+})
